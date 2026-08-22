2305426d96362bb28c54dd978d3ea353a4951621
SmartExploit <smartroyaldigitalservices@gmail.com>
2026-08-22 14:56:05 -0700

    Allow production frontend origin

--- app/main.py
+++ app/main.py
@@ -15,8 +15,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-    ],
+    "http://127.0.0.1:5500",
+    "https://phishing-message-classifier-1.onrender.com",
+],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
